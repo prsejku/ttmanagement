d0b374c7c9a32eb6863e609128ae9b15212fd20c
mfene <k1604323@students.jku.at>
2018-03-25 23:42:24 +0200

    Projekt selbst gebaut

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-
-import { AppRoutingModule } from './app.routing';
-import { ComponentsModule } from './components/components.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule, MatCheckboxModule, MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-import { TableListComponent } from './table-list/table-list.component';
-import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
+import { TimerComponent } from './timer/timer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
     UserProfileComponent,
-    TableListComponent,
-    TypographyComponent,
-    IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
-    UpgradeComponent,
-
+    TimerComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
-    ComponentsModule,
-RouterModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
