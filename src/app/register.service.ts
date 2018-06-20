9947535db90676b9848dce1a5ebf047a516fa763
mfene <markus.feneberger@gmail.com>
2018-06-20 13:51:35 +0200

    Vor Code Review

--- src/app/register.service.ts
+++ src/app/register.service.ts
@@ -1,16 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import {HttpService} from './http.service';
 import {Router} from '@angular/router';
 import {User, UserJson} from '../models/User';
-
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/delay';
-//import {TimerService} from "./timer.service";
-import {Router} from "@angular/router";
-import {User, UserJson} from "../models/User";
 
 import {MessageService} from './message.service';
 
@@ -19,7 +11,7 @@ export class RegisterService {
     progress = false;
     redirectUrl: string;
     userCreated: boolean;
-    //select * from :tabelle where :param = :body
+    // select * from :tabelle where :param = :body
 
     constructor(private http: HttpClient, public router: Router, private messageService: MessageService) {}
 
@@ -40,9 +32,9 @@ export class RegisterService {
                 this.userCreated = true;
             }
         });
-        //this.log("Successfully added the Project");
-        //return res;
-        //this.router.navigate(['/login']);
+        // this.log("Successfully added the Project");
+        // return res;
+        // this.router.navigate(['/login']);
     }
 
     private log(message: string) {
--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpService} from "../http.service";
-import {AuthService} from "../auth.service";
-import { Task } from "../../models/task"
+import {HttpService} from '../http.service';
+import {AuthService} from '../auth.service';
+import { Task } from '../../models/task';
 import {TaskService} from '../task.service';
-import {retry} from "rxjs/operators";
+import {retry} from 'rxjs/operators';
 
 @Component({
   selector: 'app-timer',
@@ -35,18 +35,18 @@ export class TimerComponent implements OnInit {
                 this.httpService.getTimeTrack(trackID).subscribe(track => {
                     this.loading = false;
                     console.log(track);
-                    //Das von JS akzeptierte Datumsformat trennt Datum und Zeit durch 'T',
-                    //Oracle trennt aber mit Leerzeichen. --> Ersetzen, um Parse zu ermöglichen
-                    let datestring = track.TASK_TIME[0].START_TIME.replace(" ", "T");
-                    let time = new Date(datestring).getMilliseconds();
-                    let timeDif = new Date(Date.now() - time);
+                    // Das von JS akzeptierte Datumsformat trennt Datum und Zeit durch 'T',
+                    // Oracle trennt aber mit Leerzeichen. --> Ersetzen, um Parse zu ermöglichen
+                    const datestring = track.TASK_TIME[0].START_TIME.replace(' ', 'T');
+                    const time = new Date(datestring).getMilliseconds();
+                    const timeDif = new Date(Date.now() - time);
                     console.log(timeDif.toISOString());
                     this.hr = timeDif.getHours();
                     this.min = timeDif.getMinutes();
                     this.sek = timeDif.getSeconds();
                     this.displayedTime = timeDif
                         .toLocaleTimeString('de')
-                        .slice(1, 8); //Abschneiden der führenden '0'
+                        .slice(1, 8); // Abschneiden der führenden '0'
                     this.timer();
                 });
             } else {
@@ -62,13 +62,18 @@ export class TimerComponent implements OnInit {
         this.interv = setInterval(_ => this.increment(), 1000);
     }
 
+    /**
+     * Inkrementiert die dargestellte Zeit ('displayedTime') um eine Sekunde und stellt dabei sicher,
+     * dass der String eine valide Zeit bleibt (Sekunden und Minuten unter 60).
+     */
     increment(): void {
         let strSek, strMin;
-        if (this.sek < 10) strSek = '0'+this.sek;
-        else strSek = this.sek.toString();
-        if (this.min < 10) strMin = '0'+this.min;
-        else strMin = this.min.toString();
-        this.displayedTime = this.hr+':'+strMin+':'+strSek;
+        // Hinzufügen einer führenden '0', wenn Sekunden und Minuten unter 10 sind
+        if (this.sek < 10) { strSek = '0' + this.sek; }
+        else { strSek = this.sek.toString(); }
+        if (this.min < 10) { strMin = '0' + this.min; }
+        else { strMin = this.min.toString(); }
+        this.displayedTime = this.hr + ':' + strMin + ':' + strSek;
         this.sek++;
         if (this.sek > 59) {
             this.sek = 0;
@@ -80,7 +85,10 @@ export class TimerComponent implements OnInit {
         }
     }
 
-    onSelect(): void {
+    /**
+     * Startet oder stoppt den Timer (aus Template heraus aufgerufen)
+     */
+    playButton(): void {
         if (!this.running) {
             this.startDbTimer();
             this.timer();
@@ -92,12 +100,16 @@ export class TimerComponent implements OnInit {
     }
 
     reset(): void {
-        this.displayedTime = "0:00:00";
+        this.displayedTime = '0:00:00';
         this.hr = 0;
         this.min = 0;
         this.sek = 0;
     }
 
+    /**
+     * Handler für die HTTP-Response aus dem HTTP Service. Bei erfolgreichem Datenbank-Eintrag wird der
+     * lokale Timer gestartet.
+     */
     startDbTimer() {
         this.httpService.startTime(this.curTask.TASK_NR, this.desc).subscribe(b => {
             console.log(b);
@@ -108,6 +120,10 @@ export class TimerComponent implements OnInit {
         });
     }
 
+    /**
+     * Handler für die HTTP-Response aus dem HTTP Service. Bei erfolgreichem Datenbank-Eintrag wird der
+     * lokale Timer gestoppt.
+     */
     stopDbTimer() {
         this.httpService.submitEndTime(new Date(Date.now())).subscribe(b => {
             console.log(b);
